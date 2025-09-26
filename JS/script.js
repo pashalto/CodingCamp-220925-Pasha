2a030efbfb11b489e806df04e346deb9742b3511
pashalto <pashaharisakhir@gmail.com>
2025-09-26 21:40:54 +0700

    Add welcome message functionality and form validation; update HTML to display username

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,21 @@
-console.log('Hello, world!');
+welcomeMessage();
+
+function welcomeMessage() {
+    let userName = prompt("Please enter your name:");
+    if (userName) {
+        document.getElementById("username").innerHTML = userName;
+    } else {
+        alert("Welcome to our website, Guest!");
+    }   
+}
+
+function validateForm() {
+    let name = document.forms["contactForm"]["name"].value;
+    let email = document.forms["contactForm"]["email"].value;
+    let message = document.forms["contactForm"]["message"].value;
+
+    console.log(name, email, message);
+    if (name === "" || email === "" || message === "") {
+        alert("All fields must be filled out");
+        return false;
+    }
